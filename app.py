e2c410dd00f3c6ecb92501238eb8cf6d727eea7e
bishal-mahat25 <mahatbishal25@gmail.com>
2025-07-30 22:37:20 +0545

    Update app.py

--- app.py
+++ app.py
@@ -56,15 +56,15 @@ async def send_request(encrypted_uid, token, url):
     try:
         edata = bytes.fromhex(encrypted_uid)
         headers = {
-            'User-Agent': "Dalvik/2.1.0 (Linux; U; Android 9; ASUS_Z01QD Build/PI)",
-            'Connection': "Keep-Alive",
-            'Accept-Encoding': "gzip",
             'Authorization': f"Bearer {token}",
-            'Content-Type': "application/x-www-form-urlencoded",
-            'Expect': "100-continue",
+            'User-Agent': "Mozilla/5.0 (Linux; Android 9; ASUS_Z01QD) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Mobile Safari/537.36",
             'X-Unity-Version': "2018.4.11f1",
+            'Accept-Encoding': "gzip",
+            'Content-Type': "application/x-www-form-urlencoded",
+            'Connection': "Keep-Alive",
+            'ReleaseVersion': "OB50",  # This line has been updated
             'X-GA': "v1 1",
-            'ReleaseVersion': "OB49"
+            'X-Requested-With': "com.dts.freefireth"
         }
         async with aiohttp.ClientSession() as session:
             async with session.post(url, data=edata, headers=headers) as response:
@@ -128,15 +128,15 @@ def make_request(encrypt, server_name, token):
             url = "https://clientbp.ggblueshark.com/GetPlayerPersonalShow"
         edata = bytes.fromhex(encrypt)
         headers = {
-            'User-Agent': "Dalvik/2.1.0 (Linux; U; Android 9; ASUS_Z01QD Build/PI)",
-            'Connection': "Keep-Alive",
-            'Accept-Encoding': "gzip",
             'Authorization': f"Bearer {token}",
-            'Content-Type': "application/x-www-form-urlencoded",
-            'Expect': "100-continue",
+            'User-Agent': "Mozilla/5.0 (Linux; Android 9; ASUS_Z01QD) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Mobile Safari/537.36",
             'X-Unity-Version': "2018.4.11f1",
+            'Accept-Encoding': "gzip",
+            'Content-Type': "application/x-www-form-urlencoded",
+            'Connection': "Keep-Alive",
+            'ReleaseVersion': "OB50",  # This line has been updated
             'X-GA': "v1 1",
-            'ReleaseVersion': "OB49"
+            'X-Requested-With': "com.dts.freefireth"
         }
         response = requests.post(url, data=edata, headers=headers, verify=False)
         hex_data = response.content.hex()
